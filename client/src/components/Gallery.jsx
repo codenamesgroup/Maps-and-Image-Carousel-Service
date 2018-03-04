3a3530979dbe1747ffdde33e864eace360e4b89b
fermi_dirac <ManueleBryan@gmail.com>
2018-03-04 15:19:34 -0800

    added styling to the images gallery

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 import reactCSS from 'reactcss';
 import PropTypes from 'prop-types';
 
-class Gallery extends React.PureComponent {
+class Gallery extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentIndex: props.initialIndex,
+		};
+	}
+
+	clickLeft = () => {
+		this.setState({
+			currentIndex: this.state.currentIndex = Math.max(this.state.currentIndex - 1, 0),
+		});
+	}
+
+	clickRight = () => {
+		this.setState({
+			currentIndex: Math.min(this.state.currentIndex + 1, this.props.photos.length - 1),
+		});
+	}
+
 	render() {
 		let styles = {
 			background: {
@@ -20,34 +39,67 @@ class Gallery extends React.PureComponent {
 			},
 			container: {
 				display: 'flex',
+				flexDirection: 'column',
 				width: '1000px',
 				height: '100%',
-				marginBottom: '10%',
-				background: 'white',
+				marginBottom: '5%',
 			},
 			content: {
 				display: 'flex',
 				flexDirection: 'column',
-				backgorund: 'red',
-				width: '1000px',
-				marginBottom: '10%',
+				background: 'white',
+				width: '100%',
+				height: '100%',
 			},
 			closeButton: {
-				right: '0',
+				marginLeft: 'auto',
+				color: 'white',
+				cursor: 'pointer',
+				fontSize: '16pt',
+				padding: '4px 8px',
+			},
+
+			imageContainer: {
+				display: 'flex',
+				justifyContent: 'space-between',
+			},
+			image: {
+				width: '100%',
+				height: '100%',
+				objectFit: 'contain',
+			},
+			leftButton: {
+				position: 'absolute',
+				alignSelf: 'start',
+				bottom: '50%',
+			},
+			rightButton: {
+				position: 'absolute',
+				alignSelf: 'end',
+				bottom: '50%',
 			}
 		};
 
+		const { photos, onClose } = this.props;
+		const { currentIndex } = this.state;
+
 		return (
 			<div style={styles.background}>
-				<div style={styles.content}>
+				<div style={styles.container}>
 
-					<div onClick={this.props.onClose}>
+					<div style={styles.closeButton}
+						onClick={onClose}>
 						exit X
 					</div>
 
-
 					<div style={styles.content}>
-						<img src={`https://s3-media4.fl.yelpcdn.com/bphoto/${this.props.photos[initialIndex].id}/o.jpg`}/>
+						<div style={styles.imageContainer}>
+							<img style={styles.image}
+								src={`https://s3-media4.fl.yelpcdn.com/bphoto/${photos[currentIndex].id}/o.jpg`}/>
+
+							<div style={styles.leftButton} onClick={this.clickLeft}> left </div>
+							<div style={styles.rightButton} onClick={this.clickRight}> right </div>
+						</div>
 					</div>
 
 
