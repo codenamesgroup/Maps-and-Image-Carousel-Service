5aef22f97f86079dfc7aff9af4c72a6997b37875
fermi_dirac <ManueleBryan@gmail.com>
2018-03-05 11:42:56 -0800

    added another test to map component

--- tests/map.react.test.jsx
+++ tests/map.react.test.jsx
@@ -4,9 +4,14 @@ import { shallowToJson } from 'enzyme-to-json';
 import { Map } from './../client/src/components/Map.jsx';
 
 describe('<Map/>', () => {
-	let component = shallow(<Map longitude={0} latitude={0}/>);
 
-	it('should render with empty business object', () => {
+	it('should render with long and lat props', () => {
+		let component = shallow(<Map longitude={0} latitude={0}/>);
+		expect(shallowToJson(component)).toMatchSnapshot();
+	});
+
+	it('should render without props', () => {
+		let component = shallow(<Map/>);
 		expect(shallowToJson(component)).toMatchSnapshot();
 	});
 });
