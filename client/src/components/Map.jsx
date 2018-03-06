e8f01f9ac1546781847213e6159b7892cb45e82f
fermi_dirac <ManueleBryan@gmail.com>
2018-03-06 11:19:08 -0800

    fixed tests

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { GOOGLE_API_KEY } from './../keys';
+
 const Map = (props) => {
 	let onClick = () => {
 		console.log(props.latitude, props.longitude);
@@ -8,13 +10,16 @@ const Map = (props) => {
 
 	let style = {
 		cursor: 'pointer',
+		width: '286px',
+		height: '135px',
 	};
+
+	let mapUri = `https://maps.googleapis.com/maps/api/staticmap?key=${GOOGLE_API_KEY}&zoom=13&size=286x135&maptype=roadmap&markers=color:red%7Clabel:S%7C${props.latitude},${props.longitude}`;
 
 	return (
 		<div>
 			<img style={style}
-				src={`https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=286x135&maptype=roadmap
-				&markers=color:blue%7Clabel:S%7C${props.latitude},${props.longitude}`}
+				src={mapUri}
 				onClick={onClick}
 			/>
 		</div>
