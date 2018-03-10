9ca9900679f89586b6b5c651aeb7e8f17102fb02
fermi_dirac <ManueleBryan@gmail.com>
2018-03-09 23:15:31 -0800

    STYLED COMPONENTS ARE THE BEST

--- client/src/components/BusinessInfo.jsx
+++ client/src/components/BusinessInfo.jsx
@@ -1,77 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styled from 'styled-components';
 import * as FontAwesome from 'react-icons/lib/fa';
 
 import { Map } from './Map.jsx';
 
-const BusinessInfo = (props) => {
-	let styles = {
-		container: {
-			margin: '0',
-			padding: '4px',
-			background: 'white',
-			fontSize: '10px',
+class BusinessInfo extends React.PureComponent {
+	render() {
 
-			heigth: '300px',
+		let styles = {
+			container: {
+				margin: '0',
+				padding: '4px',
+				background: 'white',
+				fontSize: '10px',
 
-			border: '1px solid #cccccc'
-		},
-		infoContainer: {
-			display: 'grid',
-			gridTemplateColumns: '32px 1fr',
-			padding: '8px',
-		},
-		icon: {
-			width:'18px',
-			height:'18px',
-			margin: '4px auto',
-			color: 'darkgray',
-		},
-		text: {
-			margin: 'auto 0px',
-			padding: '0',
-			fontSize: '10pt',
-		},
-		link: {
-			color: 'blue',
-			cursor: 'pointer',
-		},
-		address: {
-			margin: '0',
-			padding: '0',
-			fontSize: '10pt',
-			fontWeight: 'bold',
-		},
-	};
+				heigth: '300px',
 
-	return (
-		<div style={styles.container}>
-			<Map longitude={props.business.longitude || 0.0} latitude={props.business.latitude || 0.0}/>
+				border: '1px solid #cccccc'
+			},
+			infoContainer: {
+				display: 'grid',
+				gridTemplateColumns: '32px 1fr',
+				padding: '8px',
+			},
+			icon: {
+				width:'18px',
+				height:'18px',
+				margin: '4px auto',
+				color: 'darkgray',
+			},
+			text: {
+				margin: 'auto 0px',
+				padding: '0',
+				fontSize: '10pt',
+			},
+			link: {
+				color: 'blue',
+				cursor: 'pointer',
+			},
+			address: {
+				margin: '0',
+				padding: '0',
+				fontSize: '10pt',
+				fontWeight: 'bold',
+			},
+		};
 
-			<div style={styles.infoContainer}>
+		return (
+			<div style={styles.container}>
+				<Map longitude={this.props.business.longitude || 0.0} latitude={this.props.business.latitude || 0.0}/>
 
-				<FontAwesome.FaMapMarker style={styles.icon}/>
-				<div>
-					<p style={styles.address}>{`${props.business.address}`}</p>
-					<p style={styles.address}>{`${props.business.city}, ${props.business.state} ${props.business.postal_code}`}</p>
+				<div style={styles.infoContainer}>
+
+					<FontAwesome.FaMapMarker style={styles.icon}/>
+					<div>
+						<p style={styles.address}>{`${this.props.business.address}`}</p>
+						<p style={styles.address}>{`${this.props.business.city}, ${this.props.business.state} ${this.props.business.postal_code}`}</p>
+					</div>
+
+					<FontAwesome.FaMapSigns style={styles.icon}/>
+					<p style={styles.text}><a style={styles.link}>Get Directions</a></p>
+
+					<FontAwesome.FaPhone style={styles.icon}/>
+					<p style={styles.text}>(545) 523-7964</p> {/* @TODO: THEY DONT HAVE PHONE NUMBERS IN THE DATABASE */}
+
+					<FontAwesome.FaExternalLink style={styles.icon}/>
+					<p style={styles.text}><a style={styles.link}>{`${this.props.business.name}.com`}</a></p>
+
+					<FontAwesome.FaMobile style={styles.icon}/>
+					<p style={styles.text}><a style={styles.link}>Send to your phone</a></p>
+
 				</div>
-
-				<FontAwesome.FaMapSigns style={styles.icon}/>
-				<p style={styles.text}><a style={styles.link}>Get Directions</a></p>
-
-				<FontAwesome.FaPhone style={styles.icon}/>
-				<p style={styles.text}>(545) 523-7964</p> {/* @TODO: THEY DONT HAVE PHONE NUMBERS IN THE DATABASE */}
-
-				<FontAwesome.FaExternalLink style={styles.icon}/>
-				<p style={styles.text}><a style={styles.link}>{`${props.business.name}.com`}</a></p>
-
-				<FontAwesome.FaMobile style={styles.icon}/>
-				<p style={styles.text}><a style={styles.link}>Send to your phone</a></p>
-
 			</div>
-		</div>
-	);
+		);
+	}
 };
 
 BusinessInfo.propTypes = {
